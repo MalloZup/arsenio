bc714ff249a45f875c2daf8040d7c6595799d4d6
dmaiocchi <dmaiocchi@suse.com>
2018-11-25 15:27:27 +0100

    Split out func

--- cmd/ping.go
+++ cmd/ping.go
@@ -2,8 +2,6 @@ package cmd
 
 import (
 	"context"
-	"fmt"
-	"os"
 
 	"github.com/shurcooL/githubv4"
 	"github.com/spf13/cobra"
@@ -20,11 +18,15 @@ var pingCmd = &cobra.Command{
 	Short: "write message reminder for issues/prs forgotten since XX time",
 	Long:  `ping authors with reminder`,
 	Run: func(cmd *cobra.Command, args []string) {
-		src := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
-		)
-		httpClient := oauth2.NewClient(context.Background(), src)
-		_ = githubv4.NewClient(httpClient)
-		fmt.Printf("GITHUB_TOKEN:%s", viper.Get("GITHUB_TOKEN"))
+
 	},
+}
+
+// return a GitHubClient
+func authenticateGitHubClient() error {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: viper.Get("GITHUB_TOKEN").(string)},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+	_ = githubv4.NewClient(httpClient)
 }
